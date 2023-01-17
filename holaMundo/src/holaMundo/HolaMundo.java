5f07603b6d255c7bb824a81e1f058d7c5f9a6cb3
Kurama1450 <nestorcostaorts@gmail.com>
2023-01-17 12:13:15 +0100

    Update example

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -1,10 +1,22 @@
 package holaMundo;
 
+/**
+ * 
+ * @author nesto
+ *
+ */
+
 public class HolaMundo {
 
-	public static void main(String[] args) {
+	public static void ejemplosComentarios() {
 		System.out.println("Hola Mundo!");
-		
+		System.out.println("Hola Néstor");				
+		System.out.println("Hola Néstor Costa");				
+		System.out.println("Hola Néstor Costa Orts");	
+	}
+	
+	public static void main(String[] args) {
+		ejemplosComentarios();			
 	}
 
 }
