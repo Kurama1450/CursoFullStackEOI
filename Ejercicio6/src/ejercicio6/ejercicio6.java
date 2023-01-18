8cf4656f8d6a2908a42109e769c6ec44146f9e45
Kurama1450 <nestorcostaorts@gmail.com>
2023-01-18 14:01:25 +0100

    Commit ej6 and updates

--- Ejercicio6/src/ejercicio6/ejercicio6.java
+++ Ejercicio6/src/ejercicio6/ejercicio6.java
@@ -3,8 +3,19 @@ package ejercicio6;
 public class ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int numA = 13, numB = 9 , numC = 5 , numD = 3;
 
+		final int AUXB = numB;
+		
+		System.out.println("Valores iniciales: " + "A: " + numA + " B: " + numB + " C: " + numC + " D: " + numD);
+		numB = numC;
+		System.out.println("B toma el valor de C -> B = " + numB);
+		numC = numA;
+		System.out.println("C toma el valor de A -> C = " + numC);
+		numA = numD;
+		System.out.println("A toma el valor de D -> A = " + numA);
+		numD = AUXB;
+		System.out.println("D toma el valor de B -> D = " + numD);
 	}
 
 }
