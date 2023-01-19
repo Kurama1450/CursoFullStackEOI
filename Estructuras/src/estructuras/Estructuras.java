c84eccebed840be55c1a670858710d7222cb24ac
Kurama1450 <nestorcostaorts@gmail.com>
2023-01-19 11:51:25 +0100

    Commit new project of structures

---
+++ Estructuras/src/estructuras/Estructuras.java
@@ -0,0 +1,74 @@
+package estructuras;
+
+import java.util.Scanner;
+
+public class Estructuras {
+
+	public static void ejemploIf() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca su nombre: ");
+		
+		String nombre = sc.nextLine();
+		
+		if(nombre.equals("Fran")) {
+			System.out.println("Hola Fran , pasa un buen dia");
+		}
+			
+		System.out.println("No eres Fran , pasa un buen dia");
+		
+		sc.close();
+	}
+	
+	public static void ejemploIfElse() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca su nombre: ");
+		
+		String nombre = sc.nextLine();
+		
+		if(nombre.equals("Fran")) {
+			System.out.println("Hola Fran , pasa un buen dia");
+		} else {
+			System.out.println("No eres Fran , pasa un buen dia");
+		}
+		
+		sc.close();
+	}
+	
+	public static void ejemploIfElseIfElse() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Tira un dado y escribe que número ha salido: ");
+		int num = sc.nextInt();
+		
+		if(num == 1) {
+			System.out.println("Ha salido el numero 1");
+		} else if(num == 2) {
+			System.out.println("Ha salido el numero 2");
+		}
+		else if(num == 3) {
+			System.out.println("Ha salido el numero 3");
+		}
+		else if(num == 4) {
+			System.out.println("Ha salido el numero 4");
+		}
+		else if(num == 5) {
+			System.out.println("Ha salido el numero 5");
+		}
+		else if(num == 6) {
+			System.out.println("Ha salido el numero 6");
+		} else {
+			System.out.println("¿ Qué clase de dado tienes ?");
+		}
+		
+		sc.close();
+	}
+	
+	public static void main(String[] args) {
+		
+//		ejemploIf();
+//		ejemploIfElse();
+		ejemploIfElseIfElse();
+	}
+
+}
