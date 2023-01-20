7a5761b7d2c76b5e1ac5d1160ab76318d7e9b0e7
Kurama1450 <nestorcostaorts@gmail.com>
2023-01-20 08:16:06 +0100

    Commit new exercises

--- EjerciciosEstructuras/src/ejercicios/EjerciciosEstructuras.java
+++ EjerciciosEstructuras/src/ejercicios/EjerciciosEstructuras.java
@@ -7,7 +7,11 @@ public class EjerciciosEstructuras {
 	public static void main(String[] args) {
 //		ej1();
 //		ej2();
-		ej3();
+//		ej3();
+//		ej4();
+//		ej5();
+//		ej6();
+		ej8();
 	}
 
 	public static void ej1() {
@@ -59,5 +63,121 @@ public class EjerciciosEstructuras {
 		}
 		
 		sc.close();
+	}
+	
+	public static void ej4() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca su palabra: ");
+		
+		String palabra = sc.nextLine();
+		
+		System.out.println("Introduzca su siguiente palabra: ");
+		
+		String palabra2 = sc.nextLine();
+		
+		if(palabra == palabra2) {
+			System.out.println("Son iguales");
+		}
+		else {
+			System.out.println("No son iguales");
+		}
+		
+		sc.close();
+	}
+	
+	public static void ej5() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca su numero: ");
+		
+		int num1 = sc.nextInt();
+		
+		System.out.println("Introduzca su segundo numero: ");
+		
+		int num2 = sc.nextInt();
+		
+		int result = num1/num2;
+		
+		if(result == 0) {
+			System.out.println("Resultado negativo"+ result);
+		}
+		else {
+			System.out.println("Resultado: "+ result);
+		}
+		
+		sc.close();
+	}
+	
+	public static void ej6() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca su numero: ");
+		
+		int num1 = sc.nextInt();
+		
+		System.out.println("Introduzca su segundo numero: ");
+		
+		int num2 = sc.nextInt();
+		
+		System.out.println("Introduzca su tercera numero: ");
+		
+		int num3 = sc.nextInt();
+		
+		if(num1>num2) {
+			if(num1>num3) {
+				System.out.println("El mayor es: " +num1);
+			} else {
+				System.out.println("El mayor es: " +num3);
+			}
+		} else if (num2>num3) {
+			System.out.println("El mayor es: " +num2);
+		} else {
+			System.out.println("El mayor es: " +num3);
+		}
+		
+		sc.close();
+	}
+	
+	public static void ej7() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca sus horas: ");
+		
+		int numHoras = sc.nextInt();
+		
+		System.out.println("Introduzca sus minutos: ");
+		
+		int numMin = sc.nextInt();
+		
+		System.out.println("Introduzca sus segundos: ");
+		
+		int numSeg = sc.nextInt();
+		
+		
+	}
+	
+	public static void ej8() {
+		int num = 38;
+		
+		System.out.println((num%2==0)?"Es impar":"Es par");
+	}
+	
+	public static void ej9() {
+		
+	}
+	
+	public static void ej10() {
+		
+	}
+	
+	public static void ej11() {
+		
+	}
+	
+	public static void ej12() {
+		
 	}
 }
