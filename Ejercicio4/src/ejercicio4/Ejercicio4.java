2eb4a2d1f3c69d1ecc8145cc1286a12825c082a3
Kurama1450 <nestorcostaorts@gmail.com>
2023-01-20 08:38:33 +0100

    Commit new exercise

---
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,21 @@
+package ejercicio4;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		double num = 46;
+		double num2 = 34;
+		double num3 = 50;
+		double num4 = 69;
+		
+		System.out.print(num + " , ");
+		System.out.print(num2 + " , ");
+		System.out.print(num3 + " , ");
+		System.out.print(num4);
+		System.out.println();
+		System.out.println((num+num2)*num3/num4);
+		
+	}
+
+}
