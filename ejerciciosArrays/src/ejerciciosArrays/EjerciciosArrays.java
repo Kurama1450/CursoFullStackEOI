933b1979b5b2a8376f29d4e5ec6e96f033f45b76
Kurama1450 <nestorcostaorts@gmail.com>
2023-02-01 10:20:01 +0100

    Commit updates

--- ejerciciosArrays/src/ejerciciosArrays/EjerciciosArrays.java
+++ ejerciciosArrays/src/ejerciciosArrays/EjerciciosArrays.java
@@ -40,6 +40,42 @@ public class EjerciciosArrays {
 		System.out.println("El numero menor es: " + menorProvi);
 	}
 	
+	public static void ej13() {
+		Scanner sc = new Scanner(System.in);
+		String[][] productos = new String[10][3];// Ancho y Alto . Podemos almacenar hasta 10 productos
+		//Obtener los datos de productos
+		System.out.println("¿Cuantos productos desea almacenar? (Maximo 10)");
+		int numProductos = Integer.parseInt(sc.nextLine());
+		if(numProductos>10 || numProductos < 0) {
+			System.out.println("No se puede almacenar más de 10 productos o numeros negativos");
+			sc.close();
+			return;
+		} else { //Numero entre 0 y 10
+			for(int i = 0; i<numProductos;i++) {
+				System.out.println("Introduzca el nombre del producto " + (i+1));
+				productos[i][0]=sc.nextLine();
+				System.out.println("Introduzca el precio del producto " + (i+1));
+				productos[i][1]=sc.nextLine();
+				System.out.println("Introduzca el cantidad del producto " + (i+1));
+				productos[i][2]=sc.nextLine();
+			}
+		}
+		
+		//Mostrar los datos
+		System.out.printf("-17s%-10s%5s%10s%\n","NOMBRE","PRECIO","CANTIDAD","TOTAL");
+//		for(int i = 0; i<42;i++) {
+//			System.out.println("-");
+//		}
+		System.out.println(String.valueOf("-").repeat(42));
+		for(int i =0;i<numProductos;i++) {
+			double precio = Double.parseDouble(productos[i][1]);
+			int cantidad = Integer.parseInt(productos[i][2]);
+			double total = precio * cantidad;
+			System.out.printf("-17s%-9.2f€%5d%9.2f%",productos[i][0],productos[i][1],productos[i][2],total);
+		}
+		sc.close();
+	}
+	
 	public static void main(String[] args) {
 //		ej1();
 		ej2();
