2460ed3f74ab8232c9ff5ccfd360f885cb61d7ba
Kurama1450 <nestorcostaorts@gmail.com>
2023-02-07 12:39:21 +0100

    Commit updates

--- ficheros/src/ficheros/Ejemplos.java
+++ ficheros/src/ficheros/Ejemplos.java
@@ -4,9 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -70,19 +74,57 @@ public class Ejemplos {
 	 * @param ruta  Recibe una variable de tipo Path con la ruta del fichero
 	 * @throws IOException
 	 */
-	public static void leerFicheroJava8(Path ruta) throws IOException {
-		Files.readAllLines(ruta).forEach(l->System.out.println(l));
+	public static void leerFicheroJava8(Path ruta)   {
+		try {
+			Files.readAllLines(ruta).forEach(l->System.out.println(l));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	
+	/**
+	 * Método que lee un fichero y devuelve sus lineas en una lista
+	 * @param ruta Path con la ruta del fichero
+	 * @return Lista de cadenas, una por cada línea.
+	 */
 	public static List<String> devolverLineasJava8(Path ruta){
 		try {
-			List<String> lineas = Files.readAllLines(ruta);
+			List<String> lineas = Files.readAllLines(ruta,Charset.forName("UTF-8"));
 			return lineas;
 		} catch (IOException e) {
 			//e.printStackTrace();
 			System.out.println("No se puede acceder al fichero. Error en devolverLineasJava8");
 			return null;
+		}
+	}
+	
+	/**
+	 * Escribe un fichero machacando lo anterior
+	 * @param ruta Path con la ruta del fichero
+	 * @param lineas Lista con las líneas
+	 * @return True en caso de escritura correcta, false en caso contrario
+	 */
+	public static boolean escribirLineasJava8(Path ruta, List<String> lineas) {
+		try {
+			Files.write(ruta, lineas);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	/**
+	 * Escribe un fichero añadiendo a lo que hubiese
+	 * @param ruta Path con la ruta del fichero
+	 * @param lineas Lista con las líneas
+	 * @return True en caso de escritura correcta, false en caso contrario
+	 */
+	public static boolean anyadirLineasJava8(Path ruta, List<String> lineas) {
+		try {
+			Files.write(ruta, lineas,StandardOpenOption.APPEND);
+			return true;
+		} catch (IOException e) {
+			return false;
 		}
 	}
 	
@@ -97,26 +139,70 @@ public class Ejemplos {
 	}
 	
 	public static void leerEImprimir() {
+		int numero = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduzca un número");
 		try {
-			int numero = Integer.parseInt(sc.nextLine());  // Coge un número
-			System.out.println(numero);
+			numero = Integer.parseInt(sc.nextLine());  // Coge un número
+			//System.out.println(numero);
 		} catch (NumberFormatException e) {
 			System.out.println("No ha introducido un número correcto");
-		}  // Imprime el número
-		sc.close();
+			//System.out.println(numero);
+		} finally {  // NO SIEMPRE DEBE APARECER.
+			System.out.println(numero);
+			sc.close();
+		}
+		
 	}
 
 	public static void main(String[] args) throws IOException  {
+		
+		// Lectura de Ficheros
+		/*
 		//leerFicheroJava5("C:\\ficheros\\alumnos.txt");
 		//leerFicheroJava8("C:\\ficheros\\alumnos.txt");
 		//leerFicheroJava8(Paths.get("datos","alumnos.txt"));  // Llamada con ruta relativa independiente del Sistema Operativo		
-		List<String> datosDevueltos = devolverLineasJava8(Paths.get("C:\\ficheros\\alumnos10.txt"));
-		if(datosDevueltos!=null)
-			datosDevueltos.forEach(e->System.out.println(e));
-		System.out.println(controlarDivisionEntera(10, 0));
-		leerEImprimir();
-		System.out.println("Fin del programa");
+		List<String> datosDevueltos = devolverLineasJava8(Paths.get("C:\\ficheros\\alumnos.txt"));
+		if(datosDevueltos!=null)  // Si el fichero devuelve líneas, es decir, existe.
+			datosDevueltos.forEach(e->System.out.println(e));   // imprimo todas las líneas 
+		if(datosDevueltos!=null) { // Imprimir los que empiecen por 'M' con programación estructurada
+			for(String linea : datosDevueltos) {
+				if(linea.charAt(0)=='M') {
+					System.out.println(linea);
+				}
+			}
+		}
+		
+		if(datosDevueltos!=null) {  // Programación funcional
+			datosDevueltos.stream()
+				.filter(e->e.charAt(0)=='M')
+				.forEach(e->System.out.println(e));
+		}
+		*/
+		
+		// Escritura de ficheros
+		List<String> lineas = new ArrayList<String>();  // Declara una lista de String vacía
+		lineas.add("Línea 1");
+		lineas.add("Línea 2");
+		lineas.add("Línea 3");
+		lineas.add("Línea 4");
+		// La siguiente lista tiene el mismo contenido que lineas
+		List<String> lineas2 = Arrays.asList("Linea 5" , "Línea 6", "Línea 7", "Línea 8");
+		if(escribirLineasJava8(Paths.get("C:\\ficheros\\alumnosLineas.txt"),lineas)) {
+			System.out.println("El fichero se escribió correctamente");
+		} else {
+			System.out.println("Error al escribir en el fichero");
+		}
+		if(anyadirLineasJava8(Paths.get("C:\\ficheros\\alumnosLineas.txt"),lineas2)){
+			System.out.println("Las líneas se añadieron correctamente");
+		} else {
+			System.out.println("Error al añadir líneas al fichero");
+		}
+		
+		// Ejemplos de Excepciones
+		
+		//System.out.println(controlarDivisionEntera(10, 0));
+		//leerEImprimir();
+		//System.out.println("Fin del programa");
 	}
 }
