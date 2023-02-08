64492a7a52e15888eeda76113927fbfa8e693c47
Kurama1450 <nestorcostaorts@gmail.com>
2023-02-08 10:53:46 +0100

    Commit update

--- poo1/src/poo1/Coche.java
+++ poo1/src/poo1/Coche.java
@@ -2,87 +2,111 @@ package poo1;
 
 public class Coche {
 
-	//Propiedas o Atributos
-	private int numeroRuedas;
-	private String marca;
-	private String modelo;
-	private String matricula;
-	private double precio;
-	
-	//Declaramos los constructores
-	
-	//Constructor vacio.Permite crear un objeto sin datos
-	public Coche() {
+	// 1. propiedades o atributos
+		private int numeroRuedas;
+		private String marca;
+		private String modelo;
+		private String matricula;
+		private double precio;
 		
-	}
+		// 2. Declaramos los constructores
+		
+		// 2.1 Constructor vacio. Permite crear un objeto sin datos
+		public Coche() {
+			
+		}
 
-	//Constructor con todos los parametros
-	public Coche(int numeroRuedas, String marca, String modelo, String matricula, double precio) {
-		super();
-		this.setNumeroRuedas(numeroRuedas);
-		this.marca = marca;
-		this.modelo = modelo;
-		this.matricula=matricula;
-		this.setPrecio(precio);
-	}
+		// 2.2 Constructor con todos los parámetros
+		public Coche(int numeroRuedas, String marca, String modelo, String matricula, double precio) {
+			super();
+			this.setNumeroRuedas(numeroRuedas);
+			this.marca = marca;
+			this.modelo = modelo;
+			this.matricula = matricula;
+			this.setPrecio(precio);
+		}
+		
+		// 2.3 Crear más constructores si se desean
+		
+		public Coche(int numeroRuedas, String marca, String modelo, double precio) {
+			super();
+			this.setNumeroRuedas(numeroRuedas);
+			this.marca = marca;
+			this.modelo = modelo;
+			this.setPrecio(precio);
+			this.matricula = "0000-AAA";  // puedo dar valores por defecto
+		}
 
-	//Getters y Setters
-	
-	public int getNumeroRuedas() {//Obtener
-		return numeroRuedas;
-	}
+		
+		// 2b. Constructor de copia (Optativo*). Crea un objeto igual al que te pasan
+		public Coche(Coche c) {
+			super();
+			this.setNumeroRuedas(c.numeroRuedas);
+			this.marca = c.marca;
+			this.modelo = c.modelo;
+			this.matricula = c.matricula;
+			this.setPrecio(c.precio);
+		}
+		
+		// 3. Generar Getters and Setters	
 
-	public void setNumeroRuedas(int numeroRuedas) {//Poner
-		if(numeroRuedas == 4 || numeroRuedas ==5)
-			this.numeroRuedas = Math.abs(numeroRuedas);//Si me ponen un numero negativo lo ponen en positivo.
-		else
-			this.numeroRuedas=4;
-	}
 
-	public String getMarca() {
-		return marca;
-	}
+		public int getNumeroRuedas() {
+			return numeroRuedas;
+		}
 
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
+		public void setNumeroRuedas(int numeroRuedas) {
+			if(numeroRuedas==4 || numeroRuedas==5)
+				this.numeroRuedas = Math.abs(numeroRuedas);  // Si me ponen un número negativo lo pasa a positivo
+			else
+				this.numeroRuedas=4;
+		}
 
-	public String getModelo() {
-		return modelo;
-	}
+		public String getMarca() {
+			return marca;
+		}
 
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
+		public void setMarca(String marca) {
+			this.marca = marca;
+		}  
 
-	public String getMatricula() {
-		if(this.matricula==null)
-			return "Sin matricula";
-		else
-			return matricula;
-	}
+		public String getModelo() {
+			return modelo;
+		}
 
-	public void setMatricula(String matricula) {
-		this.matricula = matricula;
-	}
+		public void setModelo(String modelo) {
+			this.modelo = modelo;
+		}
 
-	public double getPrecio() {
-		return precio;
-	}
+		public String getMatricula() {
+			if(this.matricula==null) {
+				return "Sin matrícula";
+			} else {
+				return matricula;
+			}		
+		}
 
-	public void setPrecio(double precio) {
-		if(precio<0)
-			this.precio = 0;
-		else
-			this.precio=precio;
-	}
+		public void setMatricula(String matricula) {
+			this.matricula = matricula;
+		}
 
-	//ToString te permite decidir como quieres imprimir un objeto
-	@Override
-	public String toString() {
-		return "Coche [numeroRuedas=" + numeroRuedas + ", marca=" + marca + ", modelo=" + modelo + ", matricula="
-				+ this.getMatricula() + ", precio=" + precio + "]";
-	}
+		public double getPrecio() {
+			return precio;
+		}
+
+		public void setPrecio(double precio) {
+			if(precio<0)
+				this.precio = 0;
+			else
+				this.precio = precio;
+		}
+
+		// toString te permite decidir como quieres imprimir un objeto
+		@Override
+		public String toString() {
+			return "Coche (numeroRuedas=" + numeroRuedas + ", marca=" + marca + ", modelo=" + modelo + ", matricula="
+					+ this.getMatricula() + ", precio=" + precio + ")";
+		}
 	
 	
 	
--- poo1/src/poo1/Ejemplos.java
+++ poo1/src/poo1/Ejemplos.java
@@ -3,21 +3,46 @@ package poo1;
 public class Ejemplos {
 
 	public static void main(String[] args) {
-		Coche miCoche = new Coche();//Coche sin datos
-//		miCoche.numeroRuedas = 4;
-//		miCoche.marca = "Seat";
-//		miCoche.modelo = "Altea";
-		miCoche.setNumeroRuedas(4);
+		Coche miCoche = new Coche();  // Me crea un coche sin datos. Usa el constructor vacío
+		//miCoche.numeroRuedas = -4;
+		//miCoche.marca = "Seat";
+		//miCoche.modelo = "Altea";
+		miCoche.setNumeroRuedas(6);
 		miCoche.setMarca("Seat");
 		miCoche.setModelo("Altea");
-		miCoche.setPrecio(-9999);
-		miCoche.setMatricula("8967-POL");
-		System.out.println(miCoche);
-		System.out.println("Mi coche es un " + miCoche.getMarca() + " , " + miCoche.getModelo());
+		miCoche.setPrecio(10000);
+		miCoche.setMatricula("0000-AAA");
+		System.out.println(miCoche);  // Estamos probando el toString
+		System.out.println("Mi coche es un " + 
+				miCoche.getMarca() + " " 
+				+ miCoche.getModelo() + " Con matrícula: " + miCoche.getMatricula());
 		
-		//Vamos a probar el constructor con parámetros
-		Coche z3 = new Coche(40, "Toyota", "Z3", "1234-BBN", 20000);
+		// Vamos a probar el constructor con parámetros
+		Coche z3 = new Coche(40, "Bmw", "Z3", "1111-BBB", -20000);
+		System.out.println(z3);
+		System.out.println("Mi nuevo coche es un " + 
+				z3.getMarca() + " " 
+				+ z3.getModelo() + " Con matrícula: " + z3.getMatricula());
+		
+		Coche tercero = new Coche(4, "Mercedes", "Clase A", 15000);
+		System.out.println(tercero);
+		
+		// Vamos a probar el constructor de copia
+		Coche clonZ3 = new Coche(z3);
+		System.out.println("Imprimo Z3 clonado sin cambiar nada:");
+		System.out.println(clonZ3);
+		clonZ3.setMatricula("2222-CCC");
+		System.out.println("Imprimo Z3 original:");
+		System.out.println(z3);
+		System.out.println("Imprimo Z3 clonado y cambiada la matrícula:");
+		System.out.println(clonZ3);
+		
+		// Error tipico de un programador
+		Coche intentoClon = z3;  // Esto no crea un nuevo coche, al coche existente ahora se le puede llamar de dos formas
+		z3.setPrecio(5000);
+		intentoClon.setNumeroRuedas(5);
+		System.out.println("Voy a imprimir el intentoClon:");
+		System.out.println(intentoClon);
 		System.out.println(z3);
 	}
-
 }
