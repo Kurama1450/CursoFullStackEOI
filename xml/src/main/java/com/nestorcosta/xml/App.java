dabbafc327fd9966d20b51495cd0a043441e62b9
Kurama1450 <nestorcostaorts@gmail.com>
2023-02-16 10:24:34 +0100

    Commit updates

--- xml/src/main/java/com/nestorcosta/xml/App.java
+++ xml/src/main/java/com/nestorcosta/xml/App.java
@@ -1,6 +1,11 @@
 package com.nestorcosta.xml;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,7 +15,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import org.w3c.dom.Document;
+import com.nestorcosta.xml.entidades.Asignatura;
+import com.nestorcosta.xml.entidades.NoticiaMarca;
+import com.nestorcosta.xml.entidades.NoticiaSensacine;
 
 /**
  * Hello world!
@@ -49,8 +56,136 @@ public class App
 		}
 		
 	}
+	
+
+	
+	public static List<Asignatura> devolverAsignaturas(String nombreFichero) {
+		List<Asignatura> resultado = new ArrayList<>();
+		try {
+			File inputFile = new File(nombreFichero);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);  // Comprueba que es un XML valido
+			doc.getDocumentElement().normalize();
+			
+			NodeList nList = doc.getElementsByTagName("asignatura");
+			
+			for (int temp = 0; temp < nList.getLength(); temp++) {  // recorre las asignaturas
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					// añado cada asignatura a la lista
+					resultado.add(new Asignatura(
+							eElement.getAttribute("id"),
+							eElement.getElementsByTagName("nombre").item(0).getTextContent(),
+							eElement.getElementsByTagName("cicloFormativo").item(0).getTextContent(),
+							eElement.getElementsByTagName("curso").item(0).getTextContent(),
+							eElement.getElementsByTagName("profesor").item(0).getTextContent()					
+							));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+		
+	}
+	
+	public static List<NoticiaMarca> devolverNoticiasMarca(String web) {
+		List<NoticiaMarca> resultado = new ArrayList<>();
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(web);  // Comprueba que es un XML valido
+			doc.getDocumentElement().normalize();
+			
+			NodeList nList = doc.getElementsByTagName("item");
+			
+			for (int temp = 0; temp < nList.getLength(); temp++) {  // recorre las asignaturas
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					// añado cada asignatura a la lista
+					if(eElement.getElementsByTagName("dc:creator").getLength()>0) {
+						
+					}
+					resultado.add(new NoticiaMarca(
+							eElement.getElementsByTagName("title").item(0).getTextContent(),
+							eElement.getElementsByTagName("description").item(0).getTextContent(),
+							(eElement.getElementsByTagName("dc:creator").getLength()>0)?eElement.getElementsByTagName("dc:creator").item(0).getTextContent():"",
+							eElement.getElementsByTagName("guid").item(0).getTextContent(),
+							eElement.getElementsByTagName("pubDate").item(0).getTextContent()					
+							));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+		
+	}
+	
+	public static List<NoticiaSensacine> devolverNoticiasSensacine(String web) {
+		List<NoticiaSensacine> resultado = new ArrayList<>();
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(web);  // Comprueba que es un XML valido
+			doc.getDocumentElement().normalize();
+			
+			NodeList nList = doc.getElementsByTagName("item");
+			
+			for (int temp = 0; temp < nList.getLength(); temp++) {  // recorre las asignaturas
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					// añado cada asignatura a la lista
+					if(eElement.getElementsByTagName("author").getLength()>0) {
+						
+					}
+					resultado.add(new NoticiaSensacine(
+							eElement.getElementsByTagName("category").item(0).getTextContent(),
+							eElement.getElementsByTagName("title").item(0).getTextContent(),
+							eElement.getElementsByTagName("description").item(0).getTextContent(),
+							(eElement.getElementsByTagName("author").item(0).getAttributes().getNamedItem("name").getTextContent()),
+							eElement.getElementsByTagName("guid").item(0).getTextContent(),
+							LocalDate.parse(eElement.getElementsByTagName("pubDate").item(0).getTextContent(),DateTimeFormatter.RFC_1123_DATE_TIME)				
+							));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return resultado;
+		
+	}
+	
     public static void main( String[] args )
     {
-       leerXmlFichero("asignaturas.xml");
+    	Scanner sc = new Scanner(System.in);
+        //leerXmlFichero("asignaturas.xml");
+    	/*List<Asignatura> asignaturas = devolverAsignaturas("asignaturas.xml");
+    	asignaturas.stream()
+    		.filter(e->e.getCurso().equals("Segundo"))
+    		.forEach(e->System.out.println(e));*/
+    	
+//    	List<NoticiaMarca> noticias = devolverNoticiasMarca("https://e00-marca.uecdn.es/rss/futbol/futbol-femenino.xml");
+//    	//noticias.forEach(e->System.out.println(e));
+//    	noticias.addAll(devolverNoticiasMarca("https://e00-marca.uecdn.es/rss/futbol/primera-division.xml"));
+//    	noticias.addAll(devolverNoticiasMarca("https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml"));
+//    	noticias.addAll(devolverNoticiasMarca("https://www.informacion.es/rss/section/7034"));
+//    	System.out.println("Introduzca el filtro para las noticias:");
+//    	String filtro = sc.nextLine();
+//    	noticias.stream()
+//    		.filter(e->e.getTitle().contains(filtro))
+//    		.forEach(e->System.out.println(e.getTitle()));
+//    	
+//    	sc.close();
+    		 
+    	List<NoticiaSensacine> noticias = devolverNoticiasSensacine("https://www.sensacine.com/rss/noticias-cine.xml");
+    	noticias.forEach(e->System.out.println(e));
     }
 }
