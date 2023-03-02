f1c36fb141a08ca451d5bd0d5843e465688914a9
Kurama1450 <nestorcostaorts@gmail.com>
2023-03-02 09:12:35 +0100

    Update Harry Potter

--- harrypotter/src/main/java/com/nestorcosta/harrypotter/App.java
+++ harrypotter/src/main/java/com/nestorcosta/harrypotter/App.java
@@ -1,5 +1,9 @@
 package com.nestorcosta.harrypotter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -16,6 +20,10 @@ import com.nestorcosta.harrypotter.utilidades.SerializacionUtils;
 public class App 
 {
 	static List<Personaje> personajes = new ArrayList<Personaje>();
+	final static String URI = "jdbc:mysql://localhost:3306/harry_potter";
+	final static String USER = "root";
+	final static String PASSWORD = "";
+	public static Connection con;
 	
 	public static void ejemploSerializar() {
 		//Serialización la probamos
@@ -55,13 +63,59 @@ public class App
 		personajes.forEach(e->System.out.println(e));
 	}
 	
+	public static void poblarConexion() {
+		con = null;
+		try {
+			con = DriverManager.getConnection(URI, USER, PASSWORD);
+			System.out.println("La conexión se realizo correctamente");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("La conexión ha fallado");
+		}
+	}
+	
+	public static void poblarBbdd() {
+		
+		Statement st;
+		String sql;
+		con = null;
+		try {
+			con = DriverManager.getConnection(URI, USER, PASSWORD);
+			st = con.createStatement();
+			
+			for(Personaje personaje : personajes) {
+				sql = "INSERT INTO personaje(id,name) VALUES('" +
+						personaje.getId() + "','" +
+						personaje.getName()+
+						"');";
+				System.out.println(sql);
+				st.executeUpdate(sql);
+			}
+			
+			st.close();
+			con.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("La conexión ha fallado");
+		}
+	}
+	
     public static void main( String[] args )
     {
-    	
+    	//Proceso de Serializacion
         //obtenerDatosApi();
         //rellenarFechaNacLD();
         //ejemploSerializar();
-        ejemploDesSerializar();
-        mostrarPersonajes();
+    	
+    	//Proceso de Deserializacion
+        //ejemploDesSerializar();
+        //mostrarPersonajes();
+    	
+    	//Cargar la base de datos
+    	poblarConexion();
+    	obtenerDatosApi();
+    	rellenarFechaNacLD();
+    	poblarBbdd();
     }
 }
