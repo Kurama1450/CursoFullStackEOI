15c2e0914db72d7759dff44d4493702a7e33aff3
Kurama1450 <nestorcostaorts@gmail.com>
2023-03-08 13:06:00 +0100

    Project update

---
+++ jdbc/src/main/java/com/nestorcosta/jdbc/entidades/Tabla1.java
@@ -0,0 +1,16 @@
+package com.nestorcosta.jdbc.entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tabla1 {
+
+	private int id;
+	private String nombre;
+	
+	
+}
